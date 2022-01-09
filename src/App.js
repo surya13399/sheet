305665cc9436e03264ccf0083b1c65d8deaeb046
surya13399 <suryaskcet@gmail.com>
2022-01-09 07:19:30 -0800

    sheet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import xlsx from "xlsx";
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Table extends React.Component {
+  constructor(props) {
+     super(props)
+     this.state = {
+        products: [
+           { id: 1, Product: 'Dry Fruits', Available: 21},
+           { id: 2, Product: 'kellogs', Available: 19},
+           { id: 3, Product: 'Olive oil', Available: 16},
+           { id: 4, Product: 'Pasta', Available: 25}
+        ]
+     }
+  }
+
+  renderTableHeader() {
+     let header = Object.keys(this.state.products[0])
+     return header.map((key, index) => {
+        return <th key={index}>{key.toUpperCase()}</th>
+     })
+  }
+
+  renderTableData() {
+     return this.state.products.map((product, index) => {
+        const { id,Product,Available } = product //destructuring
+        return (
+           <tr key={id}>
+              <td>{id}</td>
+              <td>{Product}</td>
+              <td>{Available}</td>
+      
+           </tr>
+        )
+     })
+  }
+
+  render() {
+     return (
+        <div>
+           <h1 id='title'>SUPER MARKET</h1>
+           <table id='students'>
+              <tbody>
+                 <tr>{this.renderTableHeader()}</tr>
+                 {this.renderTableData()}
+              </tbody>
+           </table>
+        </div>
+     )
+  }
 }
 
-export default App;
+export default Table;
